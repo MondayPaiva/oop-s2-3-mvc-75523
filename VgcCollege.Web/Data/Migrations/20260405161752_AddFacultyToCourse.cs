ad0b9c81e4a03e0edd41ff6aeb7e657396a8a9b9
Luan Paiva <luanpaiva685@gmail.com>
2026-04-05 18:17:16 +0100

    Scaffold Branches CRUD for CA3

---
+++ VgcCollege.Web/Data/Migrations/20260405161752_AddFacultyToCourse.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VgcCollege.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFacultyToCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FacultyProfileId",
+                table: "Courses",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_FacultyProfileId",
+                table: "Courses",
+                column: "FacultyProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_FacultyProfiles_FacultyProfileId",
+                table: "Courses",
+                column: "FacultyProfileId",
+                principalTable: "FacultyProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_FacultyProfiles_FacultyProfileId",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_FacultyProfileId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "FacultyProfileId",
+                table: "Courses");
+        }
+    }
+}
