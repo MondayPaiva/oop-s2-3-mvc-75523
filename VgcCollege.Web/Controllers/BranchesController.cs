c235eab0b2bf48400e8f88bffcb75086ab7d1d29
Luan Paiva <luanpaiva685@gmail.com>
2026-04-05 20:18:31 +0100

    Secure Branches CRUD and show courses in branch details

--- VgcCollege.Web/Controllers/BranchesController.cs
+++ VgcCollege.Web/Controllers/BranchesController.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using VgcCollege.Web.Data;
 using VgcCollege.Web.Models;
 
 namespace VgcCollege.Web.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class BranchesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -19,122 +16,83 @@ namespace VgcCollege.Web.Controllers
             _context = context;
         }
 
-        // GET: Branches
         public async Task<IActionResult> Index()
         {
             return View(await _context.Branches.ToListAsync());
         }
 
-        // GET: Branches/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var branch = await _context.Branches
+                .Include(b => b.Courses)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (branch == null)
-            {
-                return NotFound();
-            }
+
+            if (branch == null) return NotFound();
 
             return View(branch);
         }
 
-        // GET: Branches/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Branches/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Address")] Branch branch)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(branch);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(branch);
+            if (!ModelState.IsValid) return View(branch);
+
+            _context.Add(branch);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Branches/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var branch = await _context.Branches.FindAsync(id);
-            if (branch == null)
-            {
-                return NotFound();
-            }
+            if (branch == null) return NotFound();
+
             return View(branch);
         }
 
-        // POST: Branches/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address")] Branch branch)
         {
-            if (id != branch.Id)
+            if (id != branch.Id) return NotFound();
+
+            if (!ModelState.IsValid) return View(branch);
+
+            try
             {
-                return NotFound();
+                _context.Update(branch);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BranchExists(branch.Id))
+                    return NotFound();
+                throw;
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(branch);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BranchExists(branch.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(branch);
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Branches/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var branch = await _context.Branches
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (branch == null)
-            {
-                return NotFound();
-            }
+            var branch = await _context.Branches.FirstOrDefaultAsync(m => m.Id == id);
+            if (branch == null) return NotFound();
 
             return View(branch);
         }
 
-        // POST: Branches/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -143,9 +101,9 @@ namespace VgcCollege.Web.Controllers
             if (branch != null)
             {
                 _context.Branches.Remove(branch);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
